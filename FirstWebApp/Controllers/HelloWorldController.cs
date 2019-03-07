c68b003d562a8fec5d6f41aad6a22f505900c079
nastia3 <anastasia.kushnir@icloud.com>
2019-03-07 14:46:41 +0200

    clas

---
+++ FirstWebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name = "John Doe", int numTimes = 1)
+        {
+            ViewData["Message"] = $"Hello, {name}";
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ FirstWebApp/Data/FirstWebAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApp.Models
+{
+    public class FirstWebAppContext : DbContext
+    {
+        public FirstWebAppContext (DbContextOptions<FirstWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstWebApp.Models.Student> Student { get; set; }
+    }
+}
